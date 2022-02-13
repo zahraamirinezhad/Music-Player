e42f5322de64c3a7b2f36e5c9a82abd7dfd6854c
Zahra Amirinezhad <zahraamiri1381216@yahoo.com>
2022-02-13 10:37:41 +0330

    Some problems in getting musics got solved

--- Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.musicplayer
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,18 +11,20 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.musicplayer.databinding.ActivityMainBinding
 import java.io.File
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    //Creating Binding Object
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var musicAdapter: MusicAdapter
 
     companion object {
-        lateinit var musicList: ArrayList<Music>
+        lateinit var MusicListMA: ArrayList<Music>
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
@@ -29,90 +32,137 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         initializeLayout()
 
-        binding.favoritesBtn.setOnClickListener({
-            startActivity(Intent(this, favorite::class.java))
-        })
+        binding.shuffleBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, Player::class.java)
+            startActivity(intent)
+        }
+        binding.favoritesBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, favorite::class.java)
+            startActivity(intent)
+        }
+        binding.playlistBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, playlist::class.java)
+            startActivity(intent)
 
-        binding.playlistBtn.setOnClickListener({
-            startActivity(Intent(this, playlist::class.java))
-        })
-
-        binding.navView.setNavigationItemSelectedListener({
+        }
+        binding.navView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.feedback -> Toast.makeText(baseContext, "FEEDBACK", Toast.LENGTH_SHORT).show()
-                R.id.settings -> Toast.makeText(baseContext, "SETTINGS", Toast.LENGTH_SHORT).show()
-                R.id.about -> Toast.makeText(baseContext, "ABOUT", Toast.LENGTH_SHORT).show()
+                R.id.feedback -> Toast.makeText(baseContext, "Feedback", Toast.LENGTH_SHORT).show()
+                R.id.settings -> Toast.makeText(baseContext, "Settings", Toast.LENGTH_SHORT).show()
+                R.id.about -> Toast.makeText(baseContext, "About", Toast.LENGTH_SHORT).show()
                 R.id.exit -> exitProcess(1)
+
             }
             true
-        })
+        }
     }
 
-//    private fun requestRuntimePermission() {
-//        if (ActivityCompat.checkSelfPermission(
-//                this,
-//                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                13
-//            )
-//        }
-//    }
-//
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == 13) {
-//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-//            } else {
-//                ActivityCompat.requestPermissions(
-//                    this,
-//                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                    13
-//                )
-//            }
-//        }
-//    }
+    //For requesting permission
+    private fun requestRuntimePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                13
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 13) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            else
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    13
+                )
+        }
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item))
             return true
-        }
         return super.onOptionsItemSelected(item)
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
+    @SuppressLint("SetTextI18n")
     private fun initializeLayout() {
 //        requestRuntimePermission()
         setTheme(R.style.coolPinkNav)
+        //Initializing Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //for nav drawer
         toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
         binding.root.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        musicList = addAllAudio()
+        MusicListMA = getAllAudio()
         binding.musicRV.setHasFixedSize(true)
         binding.musicRV.setItemViewCacheSize(13)
-        binding.musicRV.layoutManager = LinearLayoutManager(this)
-        musicAdapter = MusicAdapter(this, musicList)
+        binding.musicRV.layoutManager = LinearLayoutManager(this@MainActivity)
+        musicAdapter = MusicAdapter(this@MainActivity, MusicListMA)
         binding.musicRV.adapter = musicAdapter
+//        Toast.makeText(this,musicAdapter.itemCount.toString(),Toast.LENGTH_SHORT).show()
     }
+//    @SuppressLint("Recycle", "Range")
+//    @RequiresApi(Build.VERSION_CODES.R)
+//    private fun getAllAudio(): ArrayList<Music> {
+//        val tempList = ArrayList<Music>()
+//        val selection = MediaStore.Audio.Media.IS_MUSIC + "!=0"
+//        val projection = arrayOf(
+//            MediaStore.Audio.Media._ID,
+//            MediaStore.Audio.Media.TITLE,
+//            MediaStore.Audio.Media.ALBUM,
+//            MediaStore.Audio.Media.ARTIST,
+//            MediaStore.Audio.Media.DURATION,
+//            MediaStore.Audio.Media.DATE_ADDED,
+//
+//        )
+//
+//        //just add a space in starting of DESC i.e. "DESC" --> " DESC"
+//        val cursor = this.contentResolver.query(
+//            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null,
+//            MediaStore.Audio.Media.DATE_ADDED + " DESC", null
+//        )
+//
+//        if (cursor != null) {
+//            if (cursor.moveToFirst())
+//                do {
+//                    val titleC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+//                    val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
+//                    val albumC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+//                    val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+//                    val pathC = cursor.getString(cursor.getColumnIndex(DATA))
+//                    val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+//                    val music = Music(id = idC, title = titleC, albums = albumC, artist = artistC, path = pathC, duration = durationC)
+//                    val file = File(music.path)
+//                    if (file.exists())
+//                        tempList.add(music)
+//                } while (cursor.moveToNext())
+//                cursor.close()
+//        }
+//        return tempList
+//    }
 
     @SuppressLint("Recycle", "Range")
     @RequiresApi(Build.VERSION_CODES.R)
-    private fun addAllAudio(): ArrayList<Music> {
-        val tempMusicList = ArrayList<Music>()
-        val selection = MediaStore.Audio.Media.IS_MUSIC + " !=0"
+    private fun getAllAudio(): ArrayList<Music> {
+        val tempList = ArrayList<Music>()
+        val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
             MediaStore.Audio.Media.TITLE,
@@ -120,23 +170,27 @@ class MainActivity : AppCompatActivity() {
             MediaStore.Audio.Media.ARTIST,
             MediaStore.Audio.Media.DURATION,
             MediaStore.Audio.Media.DATE_ADDED,
-            MediaStore.Audio.Media.DATA,
-        )
-        val cursor = this.contentResolver.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null,
-            MediaStore.Audio.Media.DATE_ADDED + " DESC", null
+            MediaStore.Audio.Media.DATA
         )
 
+        val cursor = this.contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null,
+            MediaStore.Audio.Media.DATE_ADDED + " DESC", null
+        )
         if (cursor != null) {
-            if (cursor.moveToFirst()) {
+            if (cursor.moveToFirst())
                 do {
                     val titleC =
                         cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
+                            ?: "Unknown"
                     val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
+                        ?: "Unknown"
                     val albumC =
                         cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
+                            ?: "Unknown"
                     val artistC =
                         cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                            ?: "Unknown"
                     val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
                     val durationC =
                         cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
@@ -150,13 +204,11 @@ class MainActivity : AppCompatActivity() {
                         duration = durationC
                     )
                     val file = File(music.path)
-                    if (file.exists()) {
-                        tempMusicList.add(music)
-                    }
+                    if (file.exists())
+                        tempList.add(music)
                 } while (cursor.moveToNext())
-                cursor.close()
-            }
+            cursor.close()
         }
-        return tempMusicList
+        return tempList
     }
 }
