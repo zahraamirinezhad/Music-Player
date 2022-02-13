2a85a415b3b8d5ac8a3bc95a8ba8cf350dc1c6ec
Zahra Amirinezhad <zahraamiri1381216@yahoo.com>
2022-02-13 11:36:47 +0330

    Duration of musics got added

--- Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MusicAdapter.kt
+++ Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MusicAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.musicplayer
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -14,6 +16,7 @@ class MusicAdapter(private val context : Context, private val musicList : ArrayL
         val album = binding.songAlbum
         val image = binding.imgMV
         val duration = binding.songDuration
+        val root = binding.root
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicAdapter.MyHolder {
@@ -23,8 +26,12 @@ class MusicAdapter(private val context : Context, private val musicList : ArrayL
     override fun onBindViewHolder(holder: MusicAdapter.MyHolder, position: Int) {
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
-        holder.duration.text = musicList[position].duration.toString()
+        holder.duration.text = formatDuration(musicList[position].duration)
         Glide.with(context).load(musicList[position].artUri).apply(RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop()).into(holder.image)
+        holder.root.setOnClickListener{
+            val intent = Intent(context,Player::class.java)
+            ContextCompat.startActivity(context,intent,null)
+        }
     }
 
     override fun getItemCount(): Int {
