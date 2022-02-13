5b577c9072dd872faf6756d36947161a1517d5ce
Zahra Amirinezhad <zahraamiri1381216@yahoo.com>
2022-02-13 14:50:41 +0330

    Back/Next buttons got added

--- Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/Player.kt
+++ Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/Player.kt
@@ -8,13 +8,14 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.musicplayer.databinding.ActivityPlayerBinding
 
 class Player : AppCompatActivity() {
-    companion object{
-        lateinit var musicListPA : ArrayList<Music>
-        var songPosition : Int = 0
-        var mediaPlayer : MediaPlayer? = null
-        var isPlaying : Boolean = false
+    companion object {
+        lateinit var musicListPA: ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+        var isPlaying: Boolean = false
     }
-    private lateinit var binding : ActivityPlayerBinding
+
+    private lateinit var binding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.coolPink)
@@ -23,13 +24,21 @@ class Player : AppCompatActivity() {
 
         initializeLayout()
 
-        binding.playPauseBTN.setOnClickListener{
-            if(isPlaying) pauseMusic()
+        binding.playPauseBTN.setOnClickListener {
+            if (isPlaying) pauseMusic()
             else playMusic()
+        }
+
+        binding.back.setOnClickListener {
+            backNextMusic(false)
+        }
+
+        binding.next.setOnClickListener {
+            backNextMusic(true)
         }
     }
 
-    private fun setLayout(){
+    private fun setLayout() {
         Glide.with(this).load(musicListPA[songPosition].artUri).apply(
             RequestOptions().placeholder(R.drawable.music_player_icon_slash_screen).centerCrop()
         ).into(binding.songImgPA)
@@ -37,22 +46,22 @@ class Player : AppCompatActivity() {
         binding.songNamePA.text = musicListPA[songPosition].title
     }
 
-    private fun createMediaPlayer(){
+    private fun createMediaPlayer() {
         try {
-            if(mediaPlayer == null) mediaPlayer = MediaPlayer()
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
             mediaPlayer!!.reset()
             mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
             mediaPlayer!!.prepare()
             playMusic()
-        }catch (e : Exception){
+        } catch (e: Exception) {
             return
         }
     }
 
-    private fun initializeLayout(){
-        songPosition = intent.getIntExtra("index",0)
-        when(intent.getStringExtra("class")){
-            "MusicAdapter" ->{
+    private fun initializeLayout() {
+        songPosition = intent.getIntExtra("index", 0)
+        when (intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
                 musicListPA = ArrayList()
                 musicListPA.addAll(MainActivity.MusicListMA)
                 setLayout()
@@ -61,14 +70,34 @@ class Player : AppCompatActivity() {
         }
     }
 
-    private fun playMusic(){
+    private fun playMusic() {
         binding.playPauseBTN.setIconResource(R.drawable.pause_music)
         isPlaying = true
         mediaPlayer!!.start()
     }
-    private fun pauseMusic(){
+
+    private fun pauseMusic() {
         binding.playPauseBTN.setIconResource(R.drawable.play_music)
         isPlaying = false
         mediaPlayer!!.pause()
+    }
+
+    private fun backNextMusic(increment: Boolean) {
+        if (increment) {
+            if (songPosition == musicListPA.size - 1)
+                songPosition = 0
+            else
+                ++songPosition
+            setLayout()
+            createMediaPlayer()
+        } else {
+            if (songPosition == 0)
+                songPosition = musicListPA.size - 1
+            else
+                --songPosition
+            setLayout()
+            createMediaPlayer()
+        }
+
     }
 }
