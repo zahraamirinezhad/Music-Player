88b3340068ed41ff432379073c7d79ff81b52587
Zahra Amirinezhad <zahraamiri1381216@yahoo.com>
2022-02-13 18:39:52 +0330

    The problem with requestRuntimePermission got solved

--- Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ Music Player Kotlin/music player/app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -31,7 +31,9 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeLayout()
+
+        //checking for permission & if permission is granted then initializeLayout
+        if (requestRuntimePermission()) initializeLayout()
 
         binding.favoritesBtn.setOnClickListener {
             val intent = Intent(this@MainActivity, favorite::class.java)
@@ -54,40 +56,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //For requesting permission
-    private fun requestRuntimePermission() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
-            )
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                13
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 13) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-            else
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    13
-                )
-        }
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item))
             return true
@@ -97,7 +65,6 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.R)
     @SuppressLint("SetTextI18n")
     private fun initializeLayout() {
-//        requestRuntimePermission()
         setTheme(R.style.coolPinkNav)
         //Initializing Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -113,47 +80,7 @@ class MainActivity : AppCompatActivity() {
         binding.musicRV.layoutManager = LinearLayoutManager(this@MainActivity)
         musicAdapter = MusicAdapter(this@MainActivity, MusicListMA)
         binding.musicRV.adapter = musicAdapter
-//        Toast.makeText(this,musicAdapter.itemCount.toString(),Toast.LENGTH_SHORT).show()
     }
-//    @SuppressLint("Recycle", "Range")
-//    @RequiresApi(Build.VERSION_CODES.R)
-//    private fun getAllAudio(): ArrayList<Music> {
-//        val tempList = ArrayList<Music>()
-//        val selection = MediaStore.Audio.Media.IS_MUSIC + "!=0"
-//        val projection = arrayOf(
-//            MediaStore.Audio.Media._ID,
-//            MediaStore.Audio.Media.TITLE,
-//            MediaStore.Audio.Media.ALBUM,
-//            MediaStore.Audio.Media.ARTIST,
-//            MediaStore.Audio.Media.DURATION,
-//            MediaStore.Audio.Media.DATE_ADDED,
-//
-//        )
-//
-//        //just add a space in starting of DESC i.e. "DESC" --> " DESC"
-//        val cursor = this.contentResolver.query(
-//            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null,
-//            MediaStore.Audio.Media.DATE_ADDED + " DESC", null
-//        )
-//
-//        if (cursor != null) {
-//            if (cursor.moveToFirst())
-//                do {
-//                    val titleC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
-//                    val idC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID))
-//                    val albumC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM))
-//                    val artistC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST))
-//                    val pathC = cursor.getString(cursor.getColumnIndex(DATA))
-//                    val durationC = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE))
-//                    val music = Music(id = idC, title = titleC, albums = albumC, artist = artistC, path = pathC, duration = durationC)
-//                    val file = File(music.path)
-//                    if (file.exists())
-//                        tempList.add(music)
-//                } while (cursor.moveToNext())
-//                cursor.close()
-//        }
-//        return tempList
-//    }
 
     @SuppressLint("Recycle", "Range")
     @RequiresApi(Build.VERSION_CODES.R)
@@ -170,7 +97,6 @@ class MainActivity : AppCompatActivity() {
             MediaStore.Audio.Media.DATA,
             MediaStore.Audio.Media.ALBUM_ID
         )
-
         val cursor = this.contentResolver.query(
             MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null,
             MediaStore.Audio.Media.DATE_ADDED + " DESC", null
@@ -192,11 +118,11 @@ class MainActivity : AppCompatActivity() {
                     val pathC = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))
                     val durationC =
                         cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION))
-
-                    val albumID = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)).toString()
+                    val albumIdC =
+                        cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID))
+                            .toString()
                     val uri = Uri.parse("content://media/external/audio/albumart")
-                    val artUriC = Uri.withAppendedPath(uri,albumID).toString()
-
+                    val artUriC = Uri.withAppendedPath(uri, albumIdC).toString()
                     val music = Music(
                         id = idC,
                         title = titleC,
@@ -213,5 +139,43 @@ class MainActivity : AppCompatActivity() {
             cursor.close()
         }
         return tempList
+    }
+
+    //For requesting permission
+    private fun requestRuntimePermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                13
+            )
+            return false
+        }
+        return true
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 13) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                initializeLayout()
+            } else
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    13
+                )
+        }
     }
 }
